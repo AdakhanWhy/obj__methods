e6432eca6219fd5d9cbdecc6dad7d8d126fa7b3b
AdakhanWhy <openpy@mail.ru>
2023-05-03 12:11:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+// let obj1 = {
+//     name: 'one',
+//     username: 'plus',
+//     number: 123,
+//     age: '12',
+//     info: {
+//         hello: 'hello',
+//         skills: ['html', 'css', 'js', 'dom', 'react']
+//     }
+// }
+
+// // let obj2 = {
+// //     age: 20
+// // }
+
+// // let newObj = Object.assign({}, obj1, obj2)
+
+// // let objJson = JSON.stringify(obj1)
+// // newObj = JSON.parse(objJson)
+
+// // let newObj = Object.keys(obj1)
+
+// let newObj = Object.values(obj1)
+
+// console.log(newObj);
+
+
+
+
+
+
+// true
+
+// function User(name){
+//     this.name = name;
+//     this.isAdmin = false;
+// }
+
+// let user1 = new User('hammon')
+// let user2 = new User('tebeat')
+// let obj = {name: 'hola'}
+
+// console.log(user1, user2, obj);
+
+
+
+// false
+
+// let user = new function(name){
+//     this.name = name
+//     this.isAdmin = false
+// }
+
+
+// console.log(user('ogogo'));
+
+
+
+
+// let person = Object.assign({
+//     name: 'adakhan',
+//     age: 20
+// })
+
+// let newPerson = Object.assign({}, person, {
+//     work: 'undeploied'
+// })
+
+// console.log(person)
+
+
+
+// let person = {
+//     name: 'hahah'
+// }
+
+// let newPerson = {
+//     ...person,
+//     age: 20
+// }
+
+// console.log(person, newPerson);
+
+
+
+
